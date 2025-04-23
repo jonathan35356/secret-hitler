424cf40b975c5106ac29e35977c046019e243348
Camilo Munoz <mcamiloesteban67@gmail.com>
2025-04-23 11:10:40 -0500

    Vista de login

--- secret-hitler/src/router/index.js
+++ secret-hitler/src/router/index.js
@@ -1,8 +1,10 @@
 import {createRouter,createWebHistory} from 'vue-router'
 import { HelloWorld } from '../components/index'
+import Login from '../Views/Login.vue'
 
 const routes= [
-    {path: "/a", name: "helloword", component: HelloWorld}
+    {path: "/a", name: "helloword", component: HelloWorld},
+    {path: "/login", name: "login", component: Login},
 ]
 
 const router = createRouter({
